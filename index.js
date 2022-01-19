7da06c0619dae2bf65e88f940bf5d08286301d5d
iovan <t.iovan@business-keeper.com>
2022-01-19 19:18:32 +0100

    intermediate

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-import express from 'express';
+//import express from 'express';
+const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    return res.send('Received a GET HTTP method');
+});
+
+app.listen(PORT, () =>
+    console.log(`Example app listening on port ${PORT}!`),
+);
