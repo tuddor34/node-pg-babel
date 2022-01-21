0b8e84f982cab0e9bd5866d5d30ab0547c61b702
iovan <t.iovan@business-keeper.com>
2022-01-21 15:50:22 +0100

    intermediate

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/users', (req, res) => {
     return res.send('Received a GET HTTP method');
 });
 
-app.post('/', (req, res) => {
+app.post('/users', (req, res) => {
     return res.send('Received a POST HTTP method');
 });
 
-app.put('/', (req, res) => {
+app.put('/users', (req, res) => {
     return res.send('Received a PUT HTTP method');
 });
 
-app.delete('/', (req, res) => {
+app.delete('/users', (req, res) => {
     return res.send('Received a DELETE HTTP method');
 });
 
