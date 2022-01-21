dbe37f9354516b9d31097eba324f376271b10778
iovan <t.iovan@business-keeper.com>
2022-01-21 16:03:14 +0100

    intermediate

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ app.post('/users', (req, res) => {
     return res.send('Received a POST HTTP method');
 });
 
-app.put('/users', (req, res) => {
-    return res.send('Received a PUT HTTP method');
+app.put('/users/:userId', (req, res) => {
+    return res.send(`Received a PUT HTTP method on user/${req.params.userId} resource`);
 });
 
-app.delete('/users', (req, res) => {
-    return res.send('Received a DELETE HTTP method');
+app.delete('/users/:userId', (req, res) => {
+    return res.send(`Received a DELETE HTTP method on user/${req.params.userId} resource`);
 });
 
 app.listen(PORT, () =>
