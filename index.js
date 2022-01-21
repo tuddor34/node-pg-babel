105c8629dc722cce077c0a7a0407c1dcc63143a9
iovan <t.iovan@business-keeper.com>
2022-01-21 15:03:25 +0100

    add other endpoints

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ app.get('/', (req, res) => {
     return res.send('Received a GET HTTP method');
 });
 
+app.post('/', (req, res) => {
+    return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+    return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+    return res.send('Received a DELETE HTTP method');
+});
+
 app.listen(PORT, () =>
     console.log(`Example app listening on port ${PORT}!`),
 );
